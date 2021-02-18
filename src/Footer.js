382ac4d7fb90a5eee8a6c6b35e9c5617b506b014
Aashay Vanpal <arkhamknight.aashay@gmail.com>
2021-02-18 23:24:53 +0530

    change deploy

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
     render() {
         return (
             <div>
-                Footer page
+                Footer page.
                 <footer className="footer">
                     <div style={{
                         "display": "flex"
